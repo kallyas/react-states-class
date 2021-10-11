919eef090cb780e1ccc0a4b06861b0bb10998a20
Kallyas <kallyasmedia@gmail.com>
2021-10-11 13:38:40 +0300

    city component

---
+++ src/CityList.js
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { WeatherContext } from "./App";
+
+const CityList = () => {
+  const { cities } = useContext(WeatherContext);
+
+  return (
+    <table className="city-list">
+      <thead>
+        <tr>
+          <th>City</th>
+          <th>Temperature</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cities.map((city, index) => (
+          <tr key={index}>
+            <td>{city.name}</td>
+            <td>{city.temperature}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default CityList;
